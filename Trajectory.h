e0800d8c68c880b4de30e292e9e1fd76b723e435
Andrii <user12341528@gmail.com>
2021-04-29 21:45:37 +0300

    Added calculation of length and corner, added interactive points input, added multiply beams support

--- Trajectory.h
+++ Trajectory.h
@@ -1,5 +1,8 @@
 #include "Point.h"
 #include <stdlib.h>
+#include <math.h>
+
+#define PI 3.141592654
 
 void get_drop_points(struct Point begin, struct Point end, struct Point *drops, double c, double c1, int count){
 
@@ -47,4 +50,47 @@ void get_drop_points(struct Point begin, struct Point end, struct Point *drops, 
         }
         printf("Drop: (%lf, %lf)\n", drops[i].x, drops[i].y);
     }
+
+    free(coef_a);
+}
+
+double get_trajectory_length(struct Point begin, struct Point end, struct Point *drops, int count){
+
+    struct Point current_point;
+    double length = 0;
+
+    if(count == 1){
+
+        length = sqrt( pow(end.x-begin.x, 2) + pow(end.y-begin.y, 2) );
+    }
+    else{
+
+        current_point.x = begin.x;
+        current_point.y = begin.y;
+        for(int i = 0; i<count-1; i++){
+
+            length += sqrt( pow(drops[i].x-current_point.x, 2) + pow(drops[i].y-current_point.y, 2) );
+            current_point.x = drops[i].x;
+            current_point.y = drops[i].y;
+        }
+
+        length += sqrt( pow(end.x-current_point.x, 2) + pow(end.y-current_point.y, 2) );
+    }
+    printf("Length: %lf\n", length);
+
+    return length;
+}
+
+double get_corner(struct Point begin, struct Point end, struct Point *drops, int count){
+    double corner;
+    printf("begin(%lf, %lf), end(%lf, %lf), drop(%lf, %lf)\n", begin.x, begin.y, end.x, end.y, drops[0].x, drops[0].y);
+    /*corners[0] - begin corner, corners[1] - end corner*/
+    corner = atan( ( begin.y - drops[0].y ) / ( abs(drops[0].x - begin.x) ) ); 
+    
+    /*converting from radians to degrees*/ 
+    corner = (corner * 180) / PI;
+
+    //printf("begin(%lf, %lf), end(%lf, %lf)\n", corners[0], corners[1]);
+
+    return corner;
 }
--- main.c
+++ main.c
@@ -2,25 +2,43 @@
 #include <stdlib.h>
 #include <math.h>
 #include "Trajectory.h"
+#include <string.h>
 
 
 GtkWidget *window, /*main window*/
-      *count, *c, *c1, *begin_x, *begin_y, *end_x, *end_y, //entries
-      *count_label, *c_label, *c1_label, *begin_x_label, *begin_y_label, *end_x_label, *end_y_label, *results, /*labels*/ 
+      *count, *c, *c1, //entries
+      *count_label, *c_label, *c1_label, *results, *help,/*labels*/ 
       *dialog, *content_area, *drawing_area,/*dialog widgets*/
       *v_label_box, *v_entry_box, *hbox,/*box widgets*/
-      *button;/*submit button*/
+      *button, *zoom;/*submit button*/
+     
+
+struct {
+  int count;
+  double coordx[2];
+  double coordy[2];
+} draw_point;
+
+struct data{
+    struct Point begin;
+    struct Point end;
+    struct Point *drops;
+    double c;
+    double c1;
+    int count;
+};
 
 
 void on_submit_clicked(GtkWidget *widget, gpointer data);
 void on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data);
-
+void on_darea_click_event(GtkWidget *widget, GdkEventButton *event,gpointer user_data);
+void draw(cairo_t *cr, struct data my_data);
 
 int main(int argc, char **argv){
 
-  gtk_init(&argc, &argv);
+    gtk_init(&argc, &argv);
 
-  /*window configuration*/
+    /*window configuration*/
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Beam trajectory");
     gtk_widget_set_size_request(window, 350,150);
@@ -31,20 +49,12 @@ int main(int argc, char **argv){
     
 
     /*entries*/
-    begin_x = gtk_entry_new();
-    begin_y = gtk_entry_new();
-    end_x = gtk_entry_new();
-    end_y = gtk_entry_new();
     c = gtk_entry_new();
     c1 = gtk_entry_new();
     count = gtk_entry_new();
 
 
     /*labels*/
-    begin_x_label = gtk_label_new("Begin x coordinate: ");
-    begin_y_label = gtk_label_new("Begin y coordinate: ");
-    end_x_label = gtk_label_new("End x coordinate: ");
-    end_y_label = gtk_label_new("End y coordinate: ");
     c_label = gtk_label_new("Lower border: ");
     c1_label = gtk_label_new("Higher border: ");
     count_label = gtk_label_new("Number of beams: ");
@@ -56,20 +66,12 @@ int main(int argc, char **argv){
 
     /*packing labels into vertical box*/
     v_label_box = gtk_vbox_new(0,2);
-    gtk_box_pack_start(GTK_BOX(v_label_box), begin_x_label, 0,0,6.5);
-    gtk_box_pack_start(GTK_BOX(v_label_box), begin_y_label, 0,0,6.5);
-    gtk_box_pack_start(GTK_BOX(v_label_box), end_x_label, 0,0,6.5);
-    gtk_box_pack_start(GTK_BOX(v_label_box), end_y_label, 0,0,6.5);
     gtk_box_pack_start(GTK_BOX(v_label_box), c_label, 0,0,6.5);
     gtk_box_pack_start(GTK_BOX(v_label_box), c1_label, 0,0,6.5);
     gtk_box_pack_start(GTK_BOX(v_label_box), count_label, 0,0,6.5);
     
     /*packing entries and submit button into vertical box*/
     v_entry_box = gtk_vbox_new(1,0);
-    gtk_box_pack_start(GTK_BOX(v_entry_box), begin_x, 0,0,0);
-    gtk_box_pack_start(GTK_BOX(v_entry_box), begin_y, 0,0,0);
-    gtk_box_pack_start(GTK_BOX(v_entry_box), end_x, 0,0,0);
-    gtk_box_pack_start(GTK_BOX(v_entry_box), end_y, 0,0,0);
     gtk_box_pack_start(GTK_BOX(v_entry_box), c, 0,0,0);
     gtk_box_pack_start(GTK_BOX(v_entry_box), c1, 0,0,0);
     gtk_box_pack_start(GTK_BOX(v_entry_box), count, 0,0,0);
@@ -89,8 +91,8 @@ int main(int argc, char **argv){
 void on_submit_clicked(GtkWidget *widget, gpointer data){
 
     drawing_area = gtk_drawing_area_new ();
-    results = gtk_label_new("Result example");
-
+    results = gtk_label_new("Result");
+    help = gtk_label_new("Select the start and end point of the trajectory with the mouse");
 
     /*dialog configuration*/
     dialog = gtk_message_dialog_new
@@ -102,6 +104,9 @@ void on_submit_clicked(GtkWidget *widget, gpointer data){
                         NULL
     );
     gtk_window_set_title(GTK_WINDOW(dialog), "Trajectory");
+    gtk_widget_add_events(GTK_WINDOW(drawing_area), GDK_BUTTON_PRESS_MASK);
+    g_signal_connect(GTK_WINDOW(drawing_area), "button-press-event", 
+      G_CALLBACK(on_darea_click_event), NULL);   
 
 
     /*connect content area with dialog*/
@@ -111,11 +116,12 @@ void on_submit_clicked(GtkWidget *widget, gpointer data){
     /*pack drawing area and label into content area*/
     gtk_container_add(GTK_CONTAINER(content_area), drawing_area);
     gtk_container_add(GTK_CONTAINER(content_area), results);
-
+    gtk_container_add(GTK_CONTAINER(content_area), help);
 
     /*drawing area configuration*/
-    gtk_widget_set_size_request (drawing_area, 1000, 1000);
-
+    double _c = atof((char*)gtk_entry_get_text(GTK_ENTRY(c)));
+    double _c1 = atof((char*)gtk_entry_get_text(GTK_ENTRY(c1)));
+    gtk_widget_set_size_request (drawing_area, 800, 800);
 
     //background color
     GdkColor color;
@@ -123,6 +129,8 @@ void on_submit_clicked(GtkWidget *widget, gpointer data){
     gtk_widget_modify_bg ( GTK_WIDGET(drawing_area), GTK_STATE_NORMAL, &color);
 
 
+
+    
     /*When start drawing event*/
     g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(on_draw_event), NULL); 
 
@@ -130,7 +138,7 @@ void on_submit_clicked(GtkWidget *widget, gpointer data){
     //showing our widgets
     gtk_widget_show(drawing_area);
     gtk_widget_show(results);
-
+    gtk_widget_show(help);
 
     /*Run dialog*/
     gtk_dialog_run(GTK_DIALOG(dialog));
@@ -140,15 +148,18 @@ void on_submit_clicked(GtkWidget *widget, gpointer data){
 }
 
 void on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data){
-
-    double _begin_x = atof((char*)gtk_entry_get_text(GTK_ENTRY(begin_x)));  
-    double _begin_y = atof((char*)gtk_entry_get_text(GTK_ENTRY(begin_y)));
-    double _end_x = atof((char*)gtk_entry_get_text(GTK_ENTRY(end_x)));  
-    double _end_y = atof((char*)gtk_entry_get_text(GTK_ENTRY(end_y)));
     double _c = atof((char*)gtk_entry_get_text(GTK_ENTRY(c)));
     double _c1 = atof((char*)gtk_entry_get_text(GTK_ENTRY(c1)));
+    double _begin_x = draw_point.coordx[0]-400;
+    double _begin_y = 400-draw_point.coordy[0];
+    double _end_x = draw_point.coordx[1]-400;
+    double _end_y = 400-draw_point.coordy[1];
     int _count = atoi((char*)gtk_entry_get_text(GTK_ENTRY(count)));
+    double length; 
+    double corner;
 
+   // printf("begin(%lf, %lf), end(%lf, %lf)\n", _begin_x, _begin_y, _end_x, _end_y);
+    
     struct Point begin;
     begin.x = _begin_x;
     begin.y = _begin_y;
@@ -156,32 +167,53 @@ void on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data){
     struct Point end;
     end.x = _end_x;
     end.y = _end_y;
-
+  
+    draw_point.count = 0;
 
     struct Point *drops;
     drops = (struct Point*)malloc(sizeof(struct Point)*(_count-1));
-    
     get_drop_points(begin, end, drops, _c, _c1, _count);
 
-    //cairo_scale(cr, 10, 10);
+    struct data my_data = {.begin = begin, .end = end, .drops = drops, .c = _c, .c1 = _c1, .count = _count};
+    gtk_label_set_text(results, "buf");
+    draw(cr, my_data);
+    char *buf;
+    length = get_trajectory_length(begin, end, drops, _count);
+    corner = get_corner(begin, end, drops, _count);
+    asprintf(&buf, "Begin corner: %lf°, End corner %lf°, Length: %lf\nbegin(%lf, %lf), end(%lf, %lf)",corner, corner, 
+                                                                                                    length, _begin_x, _begin_y, _end_x, _end_y);
+    gtk_label_set_text(results, buf); 
+    
+    for(int i = 0; i<2;i++){
+        draw_point.coordx[i] = 0;
+        draw_point.coordy[i] = 0;
+    }
+    free(drops);
+}
 
-    cairo_translate(cr, 500, 500);
+void draw(cairo_t *cr, struct data my_data){
+
+    double _begin_x = my_data.begin.x;
+    double _begin_y = my_data.begin.y;
+    struct Point *drops = my_data.drops;
+    double _end_x = my_data.end.x;
+    double _end_y = my_data.end.y;
+    double _c = my_data.c;
+    double _c1 = my_data.c1;
+    int _count = my_data.count;   
+
+    cairo_translate(cr, 400, 400);
 
     cairo_set_source_rgb(cr, 0,0,0);
     cairo_set_line_width(cr, 1.0);
 
-    cairo_move_to(cr, -500, (-1)*_c);
-    cairo_line_to(cr, 500, (-1)*_c);
+    cairo_move_to(cr, -400, (-1)*_c);
+    cairo_line_to(cr, 400, (-1)*_c);
 
-    cairo_move_to(cr, -500, (-1)*_c1);
-    cairo_line_to(cr, 500, (-1)*_c1);
+    cairo_move_to(cr, -400, (-1)*_c1);
+    cairo_line_to(cr, 400, (-1)*_c1);
 
-    if(1000.0 / _begin_x < 10){
-
-        
-    }
-
-    cairo_move_to(cr, 10*_begin_x, 10*_begin_y*(-1));
+    cairo_move_to(cr, _begin_x, _begin_y*(-1));
     for(int i = 0; i<_count-1; i++){
 
         cairo_line_to(cr, drops[i].x, drops[i].y*(-1));
@@ -190,6 +222,17 @@ void on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data){
 
 
     cairo_stroke(cr);
+       
+}  
 
-    free(drops);    
+
+void on_darea_click_event(GtkWidget *widget, GdkEventButton *event,gpointer user_data){
+    if(draw_point.count == 1) {
+      gtk_widget_queue_draw(widget);
+    }
+    if (event->button == 1) {
+        draw_point.coordx[draw_point.count] = event->x;
+        draw_point.coordy[draw_point.count] = event->y;
+        draw_point.count++;
+    }
 }
